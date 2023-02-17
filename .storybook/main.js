a58427cac759f4f0571ca02d04e639b23824aca4
jyotipadhi1 <jyotipadhi1@gmail.com>
2023-02-17 14:22:42 +0530

    upgraded storybook option

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,17 @@
-const path = require('path')
-
-const toPath = (_path) => path.join(process.cwd(), _path)
-
 module.exports = {
-    stories: ['../**/*.stories.js'],
-    addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-    webpackFinal: async (config) => {
-        return {
-            ...config,
-            resolve: {
-                ...config.resolve,
-                alias: {
-                    ...config.resolve.alias,
-                    '@emotion/core': toPath('node_modules/@emotion/react'),
-                    '@emotion/styled': toPath('node_modules/@emotion/styled'),
-                    'emotion-theming': toPath('node_modules/@emotion/react'),
-                },
-            },
-        }
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    staticDirs: ['../public'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/preset-create-react-app',
+    ],
+    framework: '@storybook/react',
+    core: {
+        builder: '@storybook/builder-webpack5',
+    },
+    features: {
+        interactionsDebugger: true,
     },
 }
