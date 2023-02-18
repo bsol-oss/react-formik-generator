230b48f15196d4b17caeeb060519b680eb307bd6
jyotipadhi1 <jyotipadhi1@gmail.com>
2023-02-18 18:21:44 +0530

    staticDirs handled

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    staticDirs: ['../public'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
